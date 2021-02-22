3690834b1913ad8d78dce95e7c728af653b0880b
yogesh-badgujar <yogeshbadgujar@whatsadeal.com>
2021-02-22 18:49:01 +0530

    sign in to my github

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import SignIn from './SignIn';
 
 class App extends Component
 {
@@ -6,7 +7,7 @@ class App extends Component
   {
     return(
       <div>
-      Hello
+          <SignIn />
       </div>
     );
   }
